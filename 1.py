61a1129d9ab4cd2dc8cc1a3ab354e1c01367a14f
Bartosz Bryniarski <brylka@jelenia.com>
2026-01-10 09:54:20 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 wyniki = np.array([65, 70, 75, 80, 80, 85, 85, 85, 90, 95, 100, 150])
 
@@ -9,3 +10,13 @@ print("STATYSTYKA OPISOWA - PODSTAWOWE MIARY:")
 # print("Średnia arytmetyczna: " + str(np.mean(wyniki)))
 # print("Średnia arytmetyczna:", np.mean(wyniki))
 print(f"Średnia arytmetyczna: {np.mean(wyniki):.2f}")
+print(f"Mediana:              {np.median(wyniki):.2f}")
+print(f"Moda:                 {stats.mode(wyniki, keepdims=True).mode[0]}")
+
+print("MIARY ROZPROSZENIA:")
+print(f"Minimum:              {np.min(wyniki):.2f}")
+print(f"Maksimum:             {np.max(wyniki):.2f}")
+# print(f"Rozstęp:              {np.max(wyniki)-np.min(wyniki):.2f}")
+print(f"Rozstęp:              {np.ptp(wyniki):.2f}")
+print(f"Wariancja:            {np.var(wyniki, ddof=1):.2f}")
+print(f"Odchylenie std:       {np.std(wyniki, ddof=1):.2f}")
