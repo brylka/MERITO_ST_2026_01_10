76f49c2be24bd31ef2ef5f7496378cfb45fb0be0
Bartosz Bryniarski <brylka@jelenia.com>
2026-01-11 14:47:48 +0100

    Niedziela

--- 8.py
+++ 8.py
@@ -3,10 +3,13 @@ from google import genai
 
 load_dotenv()
 client = genai.Client()
+history = []
 
 while True:
     prompt = input("Ja: ")
+    history.append({"role": "user", "parts": [{"text": prompt}]})
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=prompt
+        model="gemini-2.5-flash", contents=history
     )
+    history.append({"role": "model", "parts": [{"text": response.text}]})
     print("G:", response.text)
